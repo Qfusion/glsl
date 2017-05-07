d5ba18a3855752c8a708a13aea042a7a1cadfb51
Victor Luchits <vluchits@gmail.com>
2017-05-07 23:31:24 +0300

    Revert to linear fog because sqrt fog gets too dense too quick

--- include/fog.glsl
+++ include/fog.glsl
@@ -4,7 +4,7 @@ uniform myhalf3 u_FogColor;
 
 #define FOG_TEXCOORD_STEP 1.0/256.0
 
-#define FogDensity(coord) sqrt(clamp((coord)[0],0.0,1.0))*step(FOG_TEXCOORD_STEP,(coord)[1])
+#define FogDensity(coord) (clamp((coord)[0],0.0,1.0))*step(FOG_TEXCOORD_STEP,(coord)[1])
 
 #define FOG_GEN_OUTPUT_COLOR
 #include "fog_overload.glsl"
